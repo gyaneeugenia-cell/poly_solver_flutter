58da3cb95dc2013b22428da02926dc01c4c6bd27
Eugenia Etornam Gyane <gyaneeugenia@gmail.com>
2026-01-23 09:08:37 +0000

    Trigger redeploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "298d71cb5d73a5e7534d9df3b5733970
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "bd0088d476bd6fcff8e9d1e8c79123cc",
+"flutter_bootstrap.js": "fbb5f73862ec030b773a99c98128bd12",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
