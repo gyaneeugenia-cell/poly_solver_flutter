28d79f1610660bc80987227f1a8a2b41984be191
Eugenia Etornam Gyane <gyaneeugenia@gmail.com>
2026-01-23 08:52:42 +0000

    Update vercel config and index.html

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "298d71cb5d73a5e7534d9df3b5733970
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "18bcf8e8998898b69a1f6ec7b11f60d9",
+"flutter_bootstrap.js": "bd0088d476bd6fcff8e9d1e8c79123cc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
